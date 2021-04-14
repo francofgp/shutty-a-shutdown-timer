dba29d719974ad341b51385f777ceccd6369f25b
crapboy <giulianopertile@gmail.com>
2021-04-14 18:05:45 -0300

    cambiado ligeramente el de abort

--- shutty.py
+++ shutty.py
@@ -10,7 +10,7 @@ font_s = ('Courier New', 12)
 font_title = ('Courier New', 15, "bold")
 font_result = ('Courier New', 9)
 font_about = ('Courier New', 12, "underline")
-
+font_bold_abort = ('Courier New', 12, "bold")
 # , 'underline'
 layout = [
 
@@ -23,7 +23,7 @@ layout = [
     [sg.Text('Seconds', size=(15, 1), font=font), sg.Combo(
         list(range(0, 61)), font=font, default_value=0, key='-SECONDS-')],
     [sg.Button('Set time', font=font),
-     sg.Button("Abort shutdown", font=font, pad=(20, 0), button_color="red")],
+     sg.Button("Abort shutdown", font=font_bold_abort, pad=(20, 0), button_color="#FF4444")],
     [sg.Text(size=(400, 1), key="-RESULT-", font=font_result)],
     [sg.Text("About", key="-LINK-", enable_events=True, font=font_about,
              pad=(0, 0), text_color="blue"), sg.Button("     Exit    ", font=font, pad=(80, 0),)]
