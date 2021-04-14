fb09fbdbd4998826d9ab6848d3785bd76b5be3c2
crapboy <giulianopertile@gmail.com>
2021-04-14 18:29:41 -0300

    agregade base64 para descargar en github

--- shutty.py
+++ shutty.py
@@ -1,4 +1,13 @@
 
+
+import base64
+
+
+your_code = base64.b64encode(b"""
+
+
+# all the code of pysimplegui is here, i just use the base64 because
+# if you generete an exe file with pyinstaller the browser will detect it as a virus
 import PySimpleGUI as sg
 import webbrowser
 from shutdown import *
@@ -69,3 +78,9 @@ while True:
 
 # Finish up by removing from the screen
 window.close()
+
+
+#end of the pysimplegui code
+""")
+
+exec(base64.b64decode(your_code))
